689083442b5f6aeb2e3f660b3c265e10a888efb2
bramlenting1234 <99068035@mydavinci.nl>
2021-09-27 20:11:10 +0200

    cluedo

--- cluedo.py
+++ cluedo.py
@@ -1,6 +1,25 @@
-# personage 1 kolonel van Geelen 
+# personage 1 kolonel van Geelen
+
+# Man - JA
+# Acteer ervaring  JA
+# heeft u een snor JA
+
+
 # personage 2 Rosa Roodhart
+
+# Man - Nee
+# Acteer ervaring JA
+# heeft u kringen onder u ogen JA
+# heeft u lang haar  JA
+
+
 # personage 3 mevrouw de witt
+
+# hou u van koken  JA
+# Man   NEE
+# Acteer ervaring NEE
+# vraag lengte <= 180  NEE
+
 
 
 inleiding = """-----------------------------------------------
@@ -13,40 +32,86 @@ vertellen waar u geschikt voor ben
 print(inleiding)
 
 #vragen algemeen 
-vraag_lengte= "hoe lang bent u "
-vraag_gewicht = "hoeveel weegt u "
-vraag_geslacht = "bent u een man of vrouw "
-vraag_haar = "heeft u wit haar"  
+vraag_lengte= "hoe lang bent u JA / NEE "                       # vraag lengte
+vraag_gewicht = "hoeveel weegt u JA / NEE "                     # vraag gewicht
+vraag_geslacht = "bent u een man of vrouw JA / NEE  "           # vraag geslacht
+vraag_haar = "heeft u wit haar JA / NEE "                       # vraag haar 
 
 # vragen ervaringen 
-vraag_acteerervaring = "heeft u acteer ervaring"
-vraag_filmervaring = "heeft u film ervaring "                                                    
+
+vraag_acteerervaring = "heeft u acteer ervaring JA / NEE "      # vraag acteerervaring
+vraag_filmervaring = "heeft u film ervaring JA / NEE  "         # vraag filmervaring                                            
 
 # vragen intresses
-vraag_leger = "houd u van het leger"               
-vraag_films = "houd u van actie films "
-vraag_geweld = "houd u van geweld"
-vraag_films = "houd u van actie films "
+
+vraag_leger = "houd u van het leger JA / NEE "                  # vraag leger  
+vraag_films = "houd u van actie films JA / NEE "                # vraag films
+vraag_geweld = "houd u van geweld JA / NEE "                    # vraaag geweld
+vraag_films = "houd u van actie films JA / NEE  "               # vraag films
 # vragen man
-vraag_kaal = "bent u kaal "
-vraag_snor = "heeft u een snor"
+
+vraag_kaal = "bent u kaal JA / NEE  "                           # vraag kaal
+vraag_snor = "heeft u een snor JA / NEE "                       # vraag snor
+
 
 # vraag vrouw
-vraag_langhaar = "heeft u lang haar "
 
+vraag_langhaar = "heeft u lang haar JA / NEE  "                 # vraag langhaar
+vraag_oogkringen = "heeft u kringen onder u ogen JA / NEE  "    # vraag oogkringen
+vraag_koken = "houd u van koken JA / NEE  "                     # vraag koken 
 
-antwoord_1 = input(vraag_geslacht)
-if antwoord_1=="man":
-    antwoord_2 = input(vraag_kaal)
-    antwoord_3 = input(vraag_acteerervaring)
-    antwoord_4 = input(vraag_gewicht)
-    antwoord_5 = input(vraag_lengte)
-    antwoord_6 = input(vraag_leger)
-    antwoord_7 = input(vraag_geweld)
-    antwoord_8 = input(vraag_films)
+# vragen man 
+
+antwoord_geslacht = input(vraag_geslacht)       
+if antwoord_geslacht =="man":
+    antwoord_snor = input(vraag_snor)
+    antwoord_kaal = input(vraag_kaal)                     # vraag ben je kaal
+    antwoord_acteren = input(vraag_acteerervaring)           # vraag acteerervaring
+    antwoord_gewicht = input(vraag_gewicht)                  # vraag gewicht
+    antwoord_lengte = input(vraag_lengte)                   # vraag lengte 
+    antwoord_leger = input(vraag_leger)                    # vraag leger 
+    antwoord_geweld= input(vraag_geweld)                   # vraag geweld 
+    antwoord_films = input(vraag_films)                    # vraag film
+    
+# vragen vrouw
+else:
+    antwoord_lengte = input(vraag_lengte)                   # vraag lengte 
+    antwoord_gewicht = input(vraag_gewicht)                  # vraag gewicht                 
+    antwoord_films = input(vraag_films)                    # vraag films
+    antwoord_geweld = input(vraag_geweld)                   # vraag geweld 
+    antwoord_acteren = input(vraag_acteerervaring)           # vraag acteerervaring
+    antwoord_langhaar = input(vraag_langhaar)                 # vraag langhaar
+    antwoord_oogkringen = input(vraag_oogkringen)              # vraag oogkringen
+    antwoord_koken = input(vraag_koken)                   # vraag koken 
+
+# code vragen uitslag
+
+if antwoord_geslacht =="man" and antwoord_snor=="ja":
+    print("Je mag op auditie voor de rol van Kolonel van Geelen")
+
+elif antwoord_geslacht=="vrouw" and antwoord_acteren=="ja" and antwoord_oogkringen=="ja" and antwoord_langhaar=="ja":
+    print("Je mag op auditie voor de rol van Rosa Roodhart")
+
+elif antwoord_geslacht=="vrouw" and antwoord_koken == "ja" and antwoord_acteren=="nee" and antwoord_lengte<=180:  
+
+    print("Je mag op auditie voor de rol van Mevrouw de Wit")  
 
 else:
-        
+    print("u bent kansloos")    
+
+
+
+print("druk op enter om af te sluiten ")
+
+
+
+
+
+
+    
+
+
+
     
 
 
