48ad8ae297b7bff58b1c339bcf07a5f7a913e6d1
bramlenting1234 <99068035@mydavinci.nl>
2021-09-27 14:43:19 +0200

    game

---
+++ cluedo.py
@@ -0,0 +1,56 @@
+# personage 1 kolonel van Geelen 
+# personage 2 Rosa Roodhart
+# personage 3 mevrouw de witt
+
+
+inleiding = """-----------------------------------------------
+
+beantwoord de vragen als volgt en laat het programma
+vertellen waar u geschikt voor ben 
+
+-------------------------------------------------------------------
+"""
+print(inleiding)
+
+#vragen algemeen 
+vraag_lengte= "hoe lang bent u "
+vraag_gewicht = "hoeveel weegt u "
+vraag_geslacht = "bent u een man of vrouw "
+vraag_haar = "heeft u wit haar"  
+
+# vragen ervaringen 
+vraag_acteerervaring = "heeft u acteer ervaring"
+vraag_filmervaring = "heeft u film ervaring "                                                    
+
+# vragen intresses
+vraag_leger = "houd u van het leger"               
+vraag_films = "houd u van actie films "
+vraag_geweld = "houd u van geweld"
+vraag_films = "houd u van actie films "
+# vragen man
+vraag_kaal = "bent u kaal "
+vraag_snor = "heeft u een snor"
+
+# vraag vrouw
+vraag_langhaar = "heeft u lang haar "
+
+
+antwoord_1 = input(vraag_geslacht)
+if antwoord_1=="man":
+    antwoord_2 = input(vraag_kaal)
+    antwoord_3 = input(vraag_acteerervaring)
+    antwoord_4 = input(vraag_gewicht)
+    antwoord_5 = input(vraag_lengte)
+    antwoord_6 = input(vraag_leger)
+    antwoord_7 = input(vraag_geweld)
+    antwoord_8 = input(vraag_films)
+
+else:
+        
+    
+
+
+
+
+
+ 
